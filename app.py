a2cf97aa54589caa0ca6ecb75b39e7f8e312b7b7
Sean Boots <sean.boots@gmail.com>
2021-11-17 17:00:32 -0700

    Form example submission

--- app.py
+++ app.py
@@ -4,24 +4,37 @@
 # https://scoutapm.com/blog/python-flask-tutorial-getting-started-with-flask
 from flask import Flask, render_template, request
 
+# Additional tutorials
+# https://code.visualstudio.com/docs/python/tutorial-flask
+# https://pythonspot.com/flask-web-app-with-python/
+
 app = Flask(__name__)
 # home route
+
+
 @app.route("/")
 def hello():
-    return render_template('index.html', name = 'John')  # sending a variable to the template
+    # sending a variable to the template
+    return render_template('index.html', name='form tester')
 
 # serving form web page
+
+
 @app.route("/my-form")
 def form():
     return render_template('form.html')
 
 # handling form data
+
+
 @app.route('/form-handler', methods=['POST'])
 def handle_data():
     # since we sent the data using POST, we'll use request.form
     print('Name: ', request.form['name'])
     # we can also request.values
     print('Purchases: ', request.form['purchases'])
-    return "Request received successfully!"
+    return render_template('form-complete.html')
 
-app.run(debug = True) 
+
+if __name__ == "__main__":
+    app.run(debug=True)
